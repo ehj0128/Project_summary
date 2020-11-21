8b55a8a2c1be249360e0ea862d00bf23607d1ac1
Eom hongjae <ox1351@naver.com>
2020-11-21 21:24:47 +0900

    Setting vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
 <template>
-  <v-app>
-    <core-app-bar />
-
-    <core-view />
-
-    <core-footer />
-  </v-app>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
-<script>
-  export default {
-    name: 'App',
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-    components: {
-      CoreAppBar: () => import('@/components/core/AppBar'),
-      CoreFooter: () => import('@/components/core/Footer'),
-      CoreView: () => import('@/components/core/View'),
-    },
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
   }
-</script>
+}
+</style>
