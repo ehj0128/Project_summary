d6a7269fb5757abd0e74fe06bed2c8f4559f11a0
Eom hongjae <ox1351@naver.com>
2020-11-22 21:47:49 +0900

    Home scroll up

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,9 +7,14 @@
 <script>
   export default {
     name: 'App',
-
     components: {
       CoreView: () => import('@/components/core/View'),
+    },
+    mounted () {
+      this.$vuetify.goTo(0, {
+        duration: 100,
+        offset: 0,
+      })
     },
   }
 </script>
