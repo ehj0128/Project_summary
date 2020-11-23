a2c9338075a4b64246fedc961038f436d0097b08
Eom hongjae <ox1351@naver.com>
2020-11-23 16:37:53 +0900

    STEW name change

--- src/views/ProjectList/STEW.vue
+++ src/views/ProjectList/STEW.vue
@@ -79,7 +79,7 @@
 </template>
 
 <script>
-  import StewSite from '../../components/siteview/StewSite'
+  import StewSite from '../../components/siteview/StewSite.vue'
   export default {
     components: { StewSite },
     data: () => ({
