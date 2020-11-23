666c32a8df786d4927e4bed31ca959d7a386488c
Eom hongjae <ox1351@naver.com>
2020-11-23 13:22:34 +0900

    photo template

--- src/components/FeedCard.vue
+++ src/components/FeedCard.vue
@@ -7,7 +7,6 @@
       :height="value.prominent ? 450 : 350"
       color="grey lighten-1"
       dark
-      href="#!"
     >
       <v-img
         :src="require(`@/assets/articles/${value.hero}`)"
@@ -19,36 +18,9 @@
           class="fill-height text-right ma-0"
         >
           <v-col cols="12">
-            <v-chip
-              label
-              class="mx-0 mb-2 text-uppercase"
-              color="grey darken-3"
-              text-color="white"
-              small
-              @click.stop=""
-            >
-              {{ value.category }}
-            </v-chip>
-
             <h3 class="title font-weight-bold mb-2">
               {{ value.title }}
             </h3>
-
-            <div class="caption">
-              {{ value.author }}<br>Date
-            </div>
-          </v-col>
-
-          <v-col align-self="end">
-            <v-chip
-              class="text-uppercase ma-0"
-              color="primary"
-              label
-              small
-              @click.stop=""
-            >
-              Read More
-            </v-chip>
           </v-col>
         </v-row>
       </v-img>
